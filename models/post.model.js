aa44ae4fd5b335b4aae891bd405194dee48845df
ichater <chaterizaak@gmail.com>
2020-08-06 11:22:06 +1000

    added post schema

---
+++ models/post.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+    },
+  ],
+});
+
+module.exports = Post = mongoose.model("post", postSchema);
